64d53f5ec9f27f4aff0aa4462ce33e09ee1f4fa2
Jose Antonio Lopez Ramos <76192851+Kyonyx12@users.noreply.github.com>
2023-11-27 17:07:44 -0800

    Update subirDocumentos.js

--- javascript/subirDocumentos.js
+++ javascript/subirDocumentos.js
@@ -36,7 +36,7 @@ subirDocumentos.addEventListener("submit", function (e) {
             console.log("URL de descarga: ", downloadURL);
 
             alert("Archivo cargado exitosamente.");
-            location.assign("../subirDocumentosBoot.html");
+            location.assign("subirDocumentosBoot.html");
             // Aquí puedes enviar la URL de descarga al servidor o hacer lo que necesites con ella
           });
         }
